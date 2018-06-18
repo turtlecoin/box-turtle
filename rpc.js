c7c8d036af32840f3c2c91fb8505477f688819a4
ZedPea <zp+github@zacharypalmer.co.uk>
2018-06-18 03:40:36 +0100

    Update to use walletd

--- rpc.js
+++ rpc.js
@@ -1,115 +1,157 @@
 // 2014 XDN Developers
 // 2018 TurtleCoin Developers
-function forceNumber(string) {
-  var anum = /(^\d+$)|(^\d+\.\d+$)/
-  if (typeof string == 'string' && anum.test(string)) {
-    string = Number(string);
-  }
-  return string;
+
+/* Format to two decimal places */
+function fromAtomic(num)
+{
+    return (num / 100).toFixed(2);
 }
-$(function() {
-  $.fn.serializeObject = function() {
-    var self = this,
-      json = {},
-      push_counters = {},
-      patterns = {
-        "validate": /^[a-zA-Z][a-zA-Z0-9_]*(?:\[(?:\d*|[a-zA-Z0-9_]+)\])*$/,
-        "key": /[a-zA-Z0-9_]+|(?=\[\])/g,
-        "push": /^$/,
-        "fixed": /^\d+$/,
-        "named": /^[a-zA-Z0-9_]+$/
-      };
-    this.build = function(base, key, value) {
-      base[key] = forceNumber(value);
-      return base;
+
+function toAtomic(num)
+{
+    return Math.round(num * 100);
+}
+
+function callRpc(method, params, callback)
+{
+    var url = "http://" + $("#rpcHost").val() + ":" + $("#rpcPort").val() + "/json_rpc";
+
+    var request =
+    {
+        "params" : params,
+        "jsonrpc" : "2.0",
+        "id" : "test",
+        "method" : method,
+        "password" : config.rpcPassword
     };
-    this.push_counter = function(key) {
-      if (push_counters[key] === undefined) {
-        push_counters[key] = 0;
-      }
-      return push_counters[key]++;
+    
+    var resultNode = document.getElementById("rpc-result");
+    /* Clear any previous errors */
+    resultNode.innerHTML = "";
+
+    $.ajax(
+    {
+        url: url,
+        type: "POST",
+        cache: false,
+        data: JSON.stringify(request),
+
+        success: function(result)
+        {
+            callback({success: true, result: result});
+        },
+
+        error: function(jqXHR, textStatus, errorThrown)
+        {
+            resultNode.innerHTML = "Failed to contact walletd: " + textStatus;
+            callback({success: false, result: textStatus});
+        },
+
+        dataType: "json"
+    });
+}
+
+function sendTransaction(address, amount, fee)
+{
+    var params =
+    {
+        "transfers" : [{address: address, amount: toAtomic(amount)}],
+        "fee" : toAtomic(fee),
+        "anonymity" : config.mixin
     };
-    $.each($(this).serializeArray(), function() {
-      if (!patterns.validate.test(this.name)) {
-        return;
-      }
-      var k,
-        keys = this.name.match(patterns.key),
-        merge = this.value,
-        reverse_key = this.name;
-      while ((k = keys.pop()) !== undefined) {
-        reverse_key = reverse_key.replace(new RegExp("\\[" + k + "\\]$"), '');
-        if (k.match(patterns.push)) {
-          merge = self.build([], self.push_counter(reverse_key), merge);
-        } else if (k.match(patterns.fixed)) {
-          merge = self.build([], k, merge);
-        } else if (k.match(patterns.named)) {
-          merge = self.build({}, k, merge);
+
+    var returnValue = callRpc("sendTransaction", params, function(returnValue)
+    {
+        if (returnValue.success)
+        {
+            var resultNode = document.getElementById("rpc-result");
+
+            /* See if the RPC succeeded */
+            if (returnValue.result.hasOwnProperty("error"))
+            {
+                resultNode.innerHTML = "Failed to send transaction, error: "
+                                     + returnValue.result.error.message;
+            }
+            else
+            {
+                resultNode.innerHTML = "Success sending transaction, transaction hash: "
+                                     + returnValue.result.result.transactionHash;
+            }
         }
-      }
-      json = $.extend(true, json, merge);
     });
-    return json;
-  };
+}
 
-  function parseNum(num) {
-    num = num.toString();
-    return num.substr(0, num.length - 8) + '.' + num.substr(-8);
-  }
+function getBalance()
+{
+    var returnValue = callRpc("getBalance", {}, function(returnValue)
+    {
+        if (returnValue.success)
+        {
+            var resultNode = document.getElementById("rpc-result");
 
-  function numToTurtle(num) {
-    return num * 1;
-  }
+            if (returnValue.result.hasOwnProperty("error"))
+            {
+                resultNode.innerHTML = "Failed to get balance, error: "
+                                     + returnValue.result.error.message;
+            }
+            else
+            {
+                /* eep! */
+                var json = returnValue.result.result;
 
-  function handleRequest(method, params) {
-    if (method == 'transfer') {
-      console.log('EE', params.destinations[0].amount, numToTurtle(params.destinations[0].amount));
-      params.destinations[0].amount = numToTurtle(params.destinations[0].amount);
-      params.fee = numToTurtle(params.fee);
-    }
-    return params;
-  }
-
-  function handleResult(method, result, $resultNode) {
-    if (method == 'getBalance') {
-      console.log('R', $resultNode);
-      $resultNode.html('</br>Locked: ' + parseNum(result.balance) + ' </br> Unlocked: ' + parseNum(result.unlocked_balance));
-    } else if (method == 'transfer') {
-      $resultNode.html('</br>Ok');
-    } else if (method == 'store') {
-      $resultNode.html('</br>Ok');
-    }
-  }
-
-  function callRpc(method, paramsData, $resultNode) {
-    var url = 'http://' + $('#rpcHost').val() + ':' + $('#rpcPort').val() + '/json_rpc';
-    var request = {
-      "jsonrpc": "2.0",
-      "method": method,
-      "params": paramsData.params ? handleRequest(method, paramsData.params) : {}
-    };
-    console.log('RPC CALL', url, method, request);
-    $.ajax({
-      url: url,
-      type: 'POST',
-      cache: false,
-      data: JSON.stringify(request),
-      success: function(result) {
-        console.log('RPC RESULT', result);
-        handleResult(method, result.result, $resultNode);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.error('RPC CALL FAILED', textStatus);
-        //					alert('Request to ' + url + ' failed');
-      },
-      dataType: 'json'
+                resultNode.innerHTML = "Locked: "
+                                     + fromAtomic(json.lockedAmount)
+                                     + " TRTL"
+                                     + "</br>Unlocked: "
+                                     + fromAtomic(json.availableBalance)
+                                     + " TRTL";
+            }
+        }
     });
-  }
-  $('.rpc-submit').click(function() {
-    var $baseNode = $(this).closest('.rpc-method');
-    var $resultNode = $baseNode.find('.rpc-result');
-    $resultNode.text('');
-    callRpc($(this).data('method'), $baseNode.serializeObject(), $resultNode);
-    return false;
-  });
+}
+
+
+$(document).ready(function()
+{
+    document.getElementById('rpcHost').value = config.host;
+    document.getElementById('rpcPort').value = config.port;
+
+    var resultNode = document.getElementById("rpc-result");
+
+    $('#getBalance').click(function()
+    {
+        getBalance();
+    });
+
+    $('#sendTransaction').click(function()
+    {
+        resultNode.innerHTML = "";
+
+        var address = $("#address").val();
+        var amount = $("#amount").val();
+        var fee = $("fee").val();
+
+        if (address.length != config.addressLength)
+        {
+            resultNode.innerHTML = "Address is incorrect length! Should be "
+                                 + config.addressLength + " characters.";
+            return;
+        }
+
+        if (amount < config.minAmount)
+        {
+            resultNode.innerHTML = "Amount is too small! Must be at least "
+                                 + config.minAmount + " TRTL.";
+            return;
+        }
+
+        if (fee < config.minFee)
+        {
+            resultNode.innerHTML = "Fee is too small! Must be at least "
+                                 + config.minFee + " TRTL.";
+            return;
+        }
+
+        sendTransaction(address, amount, fee);
+    });
 });
