e598b77de9714c877284153330cd528d5406e805
Watt Erikson <turtle@turtle-coin.com>
2018-08-16 00:23:48 -0700

    Enable cors

--- start.bat
+++ start.bat
@@ -6,12 +6,12 @@ set /P pass=Enter Wallet Pass:
 
 if exist scripts\%user% (
     echo file exists
-    start cmd.exe /k "scripts\turtle-service.exe" -w scripts\%user% -p %pass% --rpc-password test --daemon-address public.turtlenode.io
+    start cmd.exe /k "scripts\turtle-service.exe" -w scripts\%user% -p %pass% --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
     start "" index.html
 ) else (
     echo file doesn't exist
-	start cmd.exe /k "scripts\turtle-service.exe" -g -w scripts\%user% -p %pass% --rpc-password test --daemon-address public.turtlenode.io
+	start cmd.exe /k "scripts\turtle-service.exe" -g -w scripts\%user% -p %pass% --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
 	ping -n 3 127.0.0.1 > nul
-	start cmd.exe /k "scripts\turtle-service.exe" -w scripts\%user% -p %pass% --rpc-password test --daemon-address public.turtlenode.io
+	start cmd.exe /k "scripts\turtle-service.exe" -w scripts\%user% -p %pass% --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
     start "" index.html
 )
