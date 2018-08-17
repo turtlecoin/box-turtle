0da927dc678e32d6e0abbf8d62b6f92cc9c80fdb
rixombea <42405930+rixombea@users.noreply.github.com>
2018-08-17 23:33:56 +0000

    update binary name

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@ read -p 'Enter Wallet Pass: ' pass
 
 if [[ -e "scripts/${user}" ]];then
   echo "file exists"
-  ./scripts/turtle-service.exe -w ./scripts/${user} -p "${pass}" --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
+  ./scripts/turtle-service -w ./scripts/${user} -p ${pass} --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
   xdg-open ./scripts/index.html
 else
   echo "file doesn't exist"
-  ./scripts/turtle-service.exe -g -w ./scripts/${user} -p "${pass}" --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
+  ./scripts/turtle-service -g -w ./scripts/${user} -p ${pass} --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
   sleep 1
-  ./scripts/turtle-service.exe -w ./scripts/${user} -p "${pass}" --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
+  ./scripts/turtle-service -w ./scripts/${user} -p ${pass} --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
   xdg-open ./scripts/index.html
 fi
