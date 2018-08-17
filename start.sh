fb2cc43c01e8889deee2d17166c63b4f2913e04c
rixombea <42405930+rixombea@users.noreply.github.com>
2018-08-17 03:13:07 +0000

    create start.sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+rpc="test"
+read -p 'Enter Wallet Name: ' user
+read -p 'Enter Wallet Pass: ' pass
+
+if [[ -e "scripts/${user}" ]];then
+  echo "file exists"
+  ./scripts/turtle-service.exe -w ./scripts/${user} -p "${pass}" --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
+  xdg-open ./scripts/index.html
+else
+  echo "file doesn't exist"
+  ./scripts/turtle-service.exe -g -w ./scripts/${user} -p "${pass}" --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
+  sleep 1
+  ./scripts/turtle-service.exe -w s./scripts/${user} -p "${pass}" --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
+  xdg-open ./scripts/index.html
+fi
