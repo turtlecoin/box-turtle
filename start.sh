3c5f646da572cb66e365596ec6a035ca5940261b
rixombea <rixombea@disroot.org>
2018-08-18 08:05:35 +0700

    update start.sh script
    - run turtle-service as daemon
    - hide password & handle password with space
    - only run browser if turtle-service is running

--- start.sh
+++ start.sh
@@ -1,17 +1,45 @@
 #!/usr/bin/env bash
 
 rpc="test"
-read -p 'Enter Wallet Name: ' user
-read -p 'Enter Wallet Pass: ' pass
+node="public.turtlenode.io"
+
+trap "killall -TERM turtle-service  > /dev/null 2>&1; echo bye;exit 0" SIGINT SIGTERM
+
+# stop existing turtle-service process, if any
+killall -TERM turtle-service > /dev/null 2>&1
+sleep 1
+
+read -p "Enter Wallet Name: " user
+echo -n "Enter Wallet Password: " 
+read -s pass
 
 if [[ -e "scripts/${user}" ]];then
-  echo "file exists"
-  ./scripts/turtle-service -w ./scripts/${user} -p ${pass} --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
-  xdg-open ./scripts/index.html
+  ./scripts/turtle-service \
+      -w ./scripts/${user} \
+      -p "${pass}" \
+      --rpc-password test \
+      --daemon-address ${node} \
+      --enable-cors '*' --daemon
 else
-  echo "file doesn't exist"
-  ./scripts/turtle-service -g -w ./scripts/${user} -p ${pass} --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
+  ./scripts/turtle-service -g -w ./scripts/${user} -p "${pass}" --rpc-password test --daemon-address ${node} --enable-cors '*'
   sleep 1
-  ./scripts/turtle-service -w ./scripts/${user} -p ${pass} --rpc-password test --daemon-address public.turtlenode.io --enable-cors '*'
+  ./scripts/turtle-service \
+      -w ./scripts/${user} \
+      -p "${pass}" \
+      --rpc-password test \
+      --daemon-address ${node} \
+      --enable-cors '*' --daemon
+fi
+
+sleep 5
+
+# don't run the browser if turtle-service is failed to start (wrong pass, etc)
+SVC=$(ps ax|grep turtle-service|grep -v grep)
+if [[ ! -z "$SVC" ]];then
+  echo "turtle-service is running as daemon, hit ctrl+c to stop"
   xdg-open ./scripts/index.html
+  while true
+  do
+      sleep 2
+  done
 fi
